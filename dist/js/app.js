f4ab84557f4d411770468c6efe311b35ecb42368
peterihimire <peterihimire@gmail.com>
2020-04-18 15:25:30 +0100

    Refactored the whole code

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,7 +2,6 @@
 const navbarBtn = document.querySelector(".navbar-btn");
 const navbarLinks = document.querySelector(".navbar-links");
 const navbarOverlay = document.querySelector(".navbar-overlay");
-
 const todoFeedback = document.querySelector(".todo-feedback");
 const todoInput = document.querySelector("#todo-input");
 const todoSubmit = document.querySelector("#todo-submit");
@@ -10,11 +9,9 @@ const todoDisplay = document.querySelector("#todo-display");
 const todoBox = document.querySelector("#todo-box");
 const formWrapper = document.querySelector("#form-wrapper");
 const refreshBtn = document.querySelector("#refresh-btn");
-
 let todoItemList = [];
 let todoID = 0;
 
-// navbar event listener
 navbarBtn.addEventListener("click", () => {
   let value = navbarLinks.classList.contains("navbar-collapse");
 
@@ -30,8 +27,6 @@ navbarBtn.addEventListener("click", () => {
 });
 
 navbarOverlay.addEventListener("click", e => {
-  console.log(e.target);
-  console.log("navbar overlay has been clicked");
   let value = navbarLinks.classList.contains("navbar-collapse");
   if (value) {
     navbarLinks.classList.remove("navbar-collapse");
@@ -40,10 +35,8 @@ navbarOverlay.addEventListener("click", e => {
   }
 });
 
-//Submit TODO Form
 const submitTodoForm = () => {
   const todoValue = todoInput.value;
-  //form validation
   if (todoValue === "") {
     todoFeedback.innerHTML = `<p>Value cannot be empty</p>`;
     todoFeedback.classList.add("error");
@@ -52,9 +45,7 @@ const submitTodoForm = () => {
       todoFeedback.remove("error");
     }, 3000);
   } else {
-    // creates an object when validation is passed
     todoInput.value = "";
-
     let todo = {
       id: todoID,
       name: todoValue
@@ -64,13 +55,9 @@ const submitTodoForm = () => {
     todoItemList.push(todo);
     addTodo(todo);
     saveTodo();
-
-    console.log(todoItemList);
   }
-  console.log(todoValue);
 };
 
-//adds a new todo div to the todo card
 const addTodo = todo => {
   const div = document.createElement("div");
   div.classList.toggle("item");
@@ -78,48 +65,31 @@ const addTodo = todo => {
     <div class="item-check check-slave " data-id=${todo.id} >
       <input type="checkbox" class="todo-check" value="on"  >
     </div>
-
     <div class="item-text " >
       <p  class="item-text-p" id="todo-display">${todo.name}</p>
     </div>
-
     <div class="item-edit " >
-
       <div class="icon" id="icon-edit" data-id=${todo.id}>
         <i class="fas fa-edit" id="edit" ></i>
       </div>
       <div class="icon" id="icon-edit" data-id=${todo.id}>
         <i class="fas fa-trash" id="trash"></i>
       </div>
-
     </div>
   `;
   todoBox.insertBefore(div, formWrapper);
-  // todoBox.appendChild(div);
 };
 
-//updates when the edit icon is clicked
 const editTodo = todoitem => {
   let id = parseInt(todoitem.parentElement.dataset.id);
   let singleTodo = todoitem.parentElement.parentElement.parentElement;
-  //remove from DOM
   todoBox.removeChild(singleTodo);
-  //remove from todoItemList array
   let individualTodo = todoItemList.filter(todo => todo.id === id);
-  //populate the removed todoItem from the array to the todoInput
   todoInput.value = individualTodo[0].name;
-  //return the todo without the id & that gives us the updated todoItemList
   let tempTodo = todoItemList.filter(todo => todo.id !== id);
   todoItemList = tempTodo;
-
-  console.log(id);
-  console.log(singleTodo);
-  console.log(individualTodo);
-  console.log(tempTodo);
-  console.log(todoItemList);
 };
 
-//updates when the delete icon is clicked
 const deleteTodo = todoItem => {
   let id = parseInt(todoItem.parentElement.dataset.id);
   let singleTodo = todoItem.parentElement.parentElement.parentElement;
@@ -128,65 +98,37 @@ const deleteTodo = todoItem => {
   let tempTodo = todoItemList.filter(todo => todo.id !== id);
   todoItemList = tempTodo;
   saveTodo();
-
-  console.log(individualTodo);
-  console.log(tempTodo);
-  console.log(singleTodo);
-  console.log(todoItemList);
-  console.log(id);
 };
 
-//updates when the checkbox is clicked
 const checkTodo = todoItem => {
   let id = parseInt(todoItem.parentElement.dataset.id);
   let singleTodo = todoItem.parentElement.parentElement;
-
   let todoText = singleTodo.children[1].firstElementChild.textContent;
   let todoPtag = singleTodo.children[1].firstElementChild;
-
   todoPtag.classList.toggle("strike-through");
-
-  console.log(id);
-  console.log(singleTodo);
-  console.log(todoPtag);
-  console.log(singleTodo.children[0]);
-  console.log(todoText);
 };
 
-//to  reset the todo both from the todolist and from the local storage
 const setReset = e => {
-  console.log(e.target);
-  console.log("refresh button has been clicked");
   const todoList = todoItemList.map(todo => {
     return todo.id;
   });
 
   todoList.forEach(id => deleteTodoById(id));
-  console.log(todoList);
-  console.log(todoBox);
-  console.log(todoBox.children);
   let todoBoxChildren = todoBox.children;
   let i;
   for (i = 0; i < todoBoxChildren.length; i++) {
-    console.log(todoBoxChildren[i]);
-    //remove todos with class of item only leaving the todo-feedback and form-wrapper divs
     while (todoBoxChildren[i].classList.contains("item")) {
       todoBox.removeChild(todoBoxChildren[i]);
     }
   }
-  console.log(todoBoxChildren);
 };
 refreshBtn.addEventListener("click", setReset);
 
 const deleteTodoById = id => {
-  console.log(id);
-  console.log(todoItemList);
   todoItemList = todoItemList.filter(todo => todo.id !== id);
   saveTodo();
-  console.log(todoItemList);
 };
 
-//todo submit button
 todoSubmit.addEventListener("click", e => {
   e.preventDefault();
   submitTodoForm();
@@ -194,28 +136,22 @@ todoSubmit.addEventListener("click", e => {
 
 const setupApp = () => {
   todoItemList = getTodos();
-  console.log(todoItemList);
   populateTodo(todoItemList);
   getOneTodo();
-  // setRefresh()
 };
 
 const populateTodo = todoItemList => {
   todoItemList.forEach(todo => addTodo(todo));
 };
 
-//functions for local-storage
 const saveTodo = () => {
   localStorage.setItem("todos", JSON.stringify(todoItemList));
-  console.log(todoItemList);
 };
 
-//get one todo by ID
 const getOneTodo = id => {
   let todos = JSON.parse(localStorage.getItem("todos"));
   return todos.find(function(todo) {
     todo.id === id;
-    console.log(todo.id, id);
   });
 };
 
@@ -225,25 +161,19 @@ const getTodos = () => {
     : [];
 };
 
-// executes all these functions on app load
 document.addEventListener("DOMContentLoaded", () => {
   setupApp();
 
-  //todo-box eventListener
   todoBox.addEventListener("click", e => {
-    console.log(e.target);
     if (e.target.classList.contains("fa-edit")) {
-      console.log("edit icon has been clicked");
       editTodo(e.target);
     } else if (e.target.classList.contains("fa-trash")) {
-      console.log("the trash icon has been clicked");
       deleteTodo(e.target);
     }
   });
 
   todoBox.addEventListener("change", e => {
     if (e.target.classList.contains("todo-check")) {
-      console.log("you just clicked on the check-box");
       checkTodo(e.target);
     }
   });
